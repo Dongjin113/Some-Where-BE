89cc7d14230125c1c6668c2110ca1460fdd9767f
DongJinLee <dlehdwls21@naver.com>
2024-03-20 15:18:48 +0900

    :tada: start project

---
+++ src/main/java/com/anywhere/somewhere/SomewhereApplication.java
@@ -0,0 +1,13 @@
+package com.anywhere.somewhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomewhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomewhereApplication.class, args);
+	}
+
+}
