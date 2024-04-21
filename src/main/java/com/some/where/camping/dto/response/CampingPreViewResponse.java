11de565165fee42fcda548f89eeddd9b68cfba9e
DongJinLee <dlehdwls21@naver.com>
2024-04-21 15:00:36 +0900

    :recycle: Address Region Enum 타입으로 변경함에 의한 Dto Response 수정
    ## CampingPreViewResponse
    - FetchJoin으로 받아온 Category를 응답값에 추가
    
    ## AddressDto
    - UI의 지역명과 DB의 지역명이 다르게 표시됨으로 Enum 타입을 통해 변경하여 사용

--- src/main/java/com/some/where/camping/dto/response/CampingPreViewResponse.java
+++ src/main/java/com/some/where/camping/dto/response/CampingPreViewResponse.java
@@ -1,11 +1,15 @@
 package com.some.where.camping.dto.response;
 
+import com.some.where.camping.dto.AddressDto;
 import com.some.where.domain.camping.Camping;
-import com.some.where.domain.embedded.Address;
+import com.some.where.domain.camping.CampingCategory;
 import com.some.where.domain.embedded.Location;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -13,16 +17,18 @@ public class CampingPreViewResponse {
 
     private Long campingId;
     private String placeName;
-    private Address address;
+    private AddressDto address;
     private Location location;
     private String phoneNum;
     private String homePage;
     private String subIntro;
+    private List<String> categories = new ArrayList<>();
 
     @Builder
     public CampingPreViewResponse(
-            Long campingId, String placeName, Address address,
-            Location location, String phoneNum, String homePage, String subIntro
+            Long campingId, String placeName, AddressDto address,
+            Location location, String phoneNum, String homePage,
+            String subIntro, List<String> categories
     ) {
         this.campingId = campingId;
         this.placeName = placeName;
@@ -31,17 +37,22 @@ public class CampingPreViewResponse {
         this.phoneNum = phoneNum;
         this.homePage = homePage;
         this.subIntro = subIntro;
+        this.categories = categories;
     }
 
-    public static CampingPreViewResponse of(Camping camping) {
+    public static CampingPreViewResponse of(Camping camping, List<CampingCategory> categories) {
+        List<String> categoryList = new ArrayList<>();
+        categories.forEach(campingCategory -> categoryList.add(campingCategory.getCategory().getName()));
+
         return CampingPreViewResponse.builder()
                 .campingId(camping.getId())
                 .placeName(camping.getPlaceName())
-                .address(camping.getAddress())
+                .address(new AddressDto(camping.getAddress()))
                 .location(camping.getLocation())
                 .phoneNum(camping.getPhoneNum())
                 .homePage(camping.getHomePage())
                 .subIntro(camping.getSubIntro())
+                .categories(categoryList)
                 .build();
     }
 
